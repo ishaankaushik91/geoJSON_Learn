2f2f525d6bd5031cf3d0292cb42c188338ed488f
Ishaan Kaushik <ishaankaushik91@gmail.com>
2022-10-31 05:42:49 +0530

    Update dbConnect.js

--- geo/dbConnect.js
+++ geo/dbConnect.js
@@ -4,7 +4,7 @@ async function db_Connect()
 {
     try {
         
-        await mongoose.connect(`mongodb+srv://ishaan:Ishaan123@cluster0.wucwolj.mongodb.net/geo`);
+        await mongoose.connect(`connection_string`);
         console.log(`DB connected`);
 
     } catch (error) {
